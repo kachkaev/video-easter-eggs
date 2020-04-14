a91eb1b928b8f34883e314288ca6e9d8c568df50
Alexander Kachkaev <alexander@kachkaev.ru>
2020-04-14 02:04:03 +0100

    Improve API performance

--- src/lib/io/readAndWriteHelpers.ts
+++ src/lib/io/readAndWriteHelpers.ts
@@ -1,17 +1,37 @@
 import fs from "fs-extra";
 import { safeDump, safeLoad, safeLoadAll } from "js-yaml";
+import LRUCache from "lru-cache";
+
+const lruCache = new LRUCache<string, unknown>(1000);
+
+const getValueFromCache = async <Value = unknown>(
+  id: string,
+  fallback: () => Promise<Value>,
+): Promise<Value> => {
+  if (!lruCache.has(id)) {
+    lruCache.set(id, await fallback());
+  }
+  return lruCache.get(id) as Value;
+};
 
 export const readFromYaml = async <Value = unknown>(
   pathToFile: string,
-): Promise<Value> => safeLoad(await fs.readFile(pathToFile, "utf8"));
+): Promise<Value> =>
+  getValueFromCache(pathToFile, async () =>
+    safeLoad(await fs.readFile(pathToFile, "utf8")),
+  );
 
 export const readAllFromYaml = async <Value = unknown>(
   pathToFile: string,
-): Promise<Value[]> => safeLoadAll(await fs.readFile(pathToFile, "utf8"));
+): Promise<Value[]> =>
+  getValueFromCache(pathToFile, async () =>
+    safeLoadAll(await fs.readFile(pathToFile, "utf8")),
+  );
 
 export const readFromJson = async <Value = unknown>(
   pathToFile: string,
-): Promise<Value> => fs.readJson(pathToFile);
+): Promise<Value> =>
+  getValueFromCache(pathToFile, async () => await fs.readJson(pathToFile));
 
 export const writeToYaml = async <Value = unknown>(
   pathToFile: string,
