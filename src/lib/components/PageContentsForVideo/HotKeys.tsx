5760c532ad8e8fc4b80e6ebe7960ed17e73d4fed
Alexander Kachkaev <alexander@kachkaev.ru>
2020-04-18 17:43:17 +0100

    Extract hot keys into a separate component

---
+++ src/lib/components/PageContentsForVideo/HotKeys.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useHotkeys } from "react-hotkeys-hook";
+
+import { VideoInfo } from "../../resources/videos";
+import { generateVideoUrl } from "../../ui";
+
+interface HotKeysProps {
+  activeTimeOffset: number;
+  onActiveTimeOffsetChange: React.Dispatch<React.SetStateAction<number>>;
+  videoInfo: VideoInfo;
+}
+
+const HotKeys: React.FunctionComponent<HotKeysProps> = ({
+  activeTimeOffset,
+  onActiveTimeOffsetChange,
+  videoInfo,
+}) => {
+  const activeSegmentIndex = videoInfo.labeledSections.findIndex(
+    (section) =>
+      section.timeOffset <= activeTimeOffset &&
+      section.timeOffset + section.timeDuration > activeTimeOffset,
+  );
+
+  useHotkeys("left,shift+left,right,shift+right", (event) => {
+    event.preventDefault();
+    const delta =
+      (event.shiftKey ? 10000 : videoInfo.frameSamplingInterval) *
+      (event.key === "ArrowLeft" ? -1 : 1);
+    onActiveTimeOffsetChange((timeOffset) =>
+      Math.min(Math.max(timeOffset + delta, 0), videoInfo.processedDuration),
+    );
+  });
+
+  useHotkeys(
+    "up,shift+up,down,shift+down",
+    (event) => {
+      event.preventDefault();
+      const delta =
+        (event.shiftKey ? 5 : 1) * (event.key === "ArrowUp" ? -1 : 1);
+      const activeTimeOffsetWithinActiveSegment =
+        activeTimeOffset -
+        videoInfo.labeledSections[activeSegmentIndex].timeOffset;
+      const newSegment = videoInfo.labeledSections[activeSegmentIndex + delta];
+      if (newSegment) {
+        onActiveTimeOffsetChange(
+          newSegment.timeOffset +
+            Math.min(
+              activeTimeOffsetWithinActiveSegment,
+              newSegment.timeDuration - videoInfo.frameSamplingInterval,
+            ),
+        );
+      }
+    },
+    {},
+    [activeSegmentIndex, activeTimeOffset, videoInfo],
+  );
+
+  useHotkeys(
+    "enter",
+    (event) => {
+      event.preventDefault();
+      const newWindow = window.open(
+        generateVideoUrl(videoInfo, activeTimeOffset),
+        "_blank",
+      );
+      newWindow?.focus();
+    },
+    {},
+    [activeTimeOffset, videoInfo],
+  );
+  return null;
+};
+
+export default HotKeys;
--- src/lib/components/PageContentsForVideo/VideoVaUi.tsx
+++ src/lib/components/PageContentsForVideo/VideoVaUi.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { useHotkeys } from "react-hotkeys-hook";
 import { useSize } from "react-use";
 import { FixedSizeList } from "react-window";
 import styled from "styled-components";
 
 import { VideoInfo } from "../../resources/videos/types";
-import { generateVideoUrl } from "../../ui";
 import ActiveFrameDetails from "./ActiveFrameDetails";
+import HotKeys from "./HotKeys";
 import TimelineSection, { TimelineSectionData } from "./TimelineSection";
 
 const Wrapper = styled.div`
@@ -43,64 +42,11 @@ const VideoVaUi: React.FunctionComponent<{
     return result;
   }, [labeledSections]);
 
-  const activeSegmentIndex = labeledSections.findIndex(
-    (section) =>
-      section.timeOffset <= activeTimeOffset &&
-      section.timeOffset + section.timeDuration > activeTimeOffset,
-  );
-
-  useHotkeys("left,shift+left,right,shift+right", (event) => {
-    event.preventDefault();
-    const delta =
-      (event.shiftKey ? 10000 : videoInfo.frameSamplingInterval) *
-      (event.key === "ArrowLeft" ? -1 : 1);
-    setActiveTimeOffset((timeOffset) =>
-      Math.min(Math.max(timeOffset + delta, 0), videoInfo.processedDuration),
-    );
-  });
-
-  useHotkeys(
-    "up,shift+up,down,shift+down",
-    (event) => {
-      event.preventDefault();
-      const delta =
-        (event.shiftKey ? 5 : 1) * (event.key === "ArrowUp" ? -1 : 1);
-      const activeTimeOffsetWithinActiveSegment =
-        activeTimeOffset - labeledSections[activeSegmentIndex].timeOffset;
-      const newSegment = labeledSections[activeSegmentIndex + delta];
-      if (newSegment) {
-        setActiveTimeOffset(
-          newSegment.timeOffset +
-            Math.min(
-              activeTimeOffsetWithinActiveSegment,
-              newSegment.timeDuration - videoInfo.frameSamplingInterval,
-            ),
-        );
-      }
-    },
-    {},
-    [activeSegmentIndex, activeTimeOffset, labeledSections, videoInfo],
-  );
-
-  useHotkeys(
-    "enter",
-    (event) => {
-      event.preventDefault();
-      const newWindow = window.open(
-        generateVideoUrl(videoInfo, activeTimeOffset),
-        "_blank",
-      );
-      newWindow?.focus();
-    },
-    {},
-    [activeTimeOffset, videoInfo],
-  );
-
   const timelineSectionData: TimelineSectionData = {
     activeTimeOffset,
     frameStripeWidth,
-    onActiveTimeOffsetChange: setActiveTimeOffset,
     maxLabeledSectionDuration,
+    onActiveTimeOffsetChange: setActiveTimeOffset,
     videoInfo,
   };
 
@@ -123,6 +69,11 @@ const VideoVaUi: React.FunctionComponent<{
 
   return (
     <Wrapper>
+      <HotKeys
+        activeTimeOffset={activeTimeOffset}
+        onActiveTimeOffsetChange={setActiveTimeOffset}
+        videoInfo={videoInfo}
+      />
       <ActiveFrameDetails
         videoInfo={videoInfo}
         activeTimeOffset={activeTimeOffset}
